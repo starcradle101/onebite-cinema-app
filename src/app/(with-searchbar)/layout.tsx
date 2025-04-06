65b474d5462333a8f9731bc1aa1801e2411be153
starcradle101 <dreamerdev12@gmail.com>
2025-04-06 17:27:47 +0900

    feat(day9): 9일차 미션 완료

---
+++ src/app/(with-searchbar)/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<div>
+			<div>SearchBar Layout</div>
+			{children}
+		</div>
+	);
+}
---
+++ src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page({
+	searchParams,
+}: {
+	searchParams: Promise<{ q: string }>;
+}) {
+	const { q } = await searchParams;
+
+	return <div>Search Page: {q}</div>;
+}
