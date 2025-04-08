3a30a2be043fa311661ec5a8da519a17a84bf96c
starcradle101 <dreamerdev12@gmail.com>
2025-04-09 02:40:08 +0900

    refactor(day12): 전체 영화 fetch API 캐싱 전략 수정

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -5,7 +5,7 @@ import { MovieData } from '@/types';
 async function AllMovies() {
 	const response = await fetch(
 		`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`,
-		{ cache: 'force-cache' }
+		{ next: { revalidate: 60 * 60 } }
 	);
 	if (!response.ok) {
 		return <div>오류가 발생했습니다...</div>;
@@ -24,7 +24,7 @@ async function AllMovies() {
 async function RecommendedMovies() {
 	const response = await fetch(
 		`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`,
-		{ next: { revalidate: 3600 } }
+		{ next: { revalidate: 60 * 60 } }
 	);
 	if (!response.ok) {
 		return <div>오류가 발생했습니다...</div>;
